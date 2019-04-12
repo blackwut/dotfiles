ad4e5412665ce1ee38236a55a1c0ee1859c87911
blackwut <albertoottimo@gmail.com>
2019-04-12 13:44:43 +0200

    duti now loads a configuration file

--- brew_install.sh
+++ brew_install.sh
@@ -10,35 +10,53 @@ brew install bash-completion@2
 brew install git
 brew install gpg
 brew install macvim
+brew install tmux
+brew install htop
 brew install duti
-brew cask install java
+brew install python
+
+# Utilities
 brew cask install smcfancontrol
-brew cask install sublime-text
-brew cask install google-chrome
-brew cask install opera
-brew cask install discord
+brew cask install gfxcardstatus
+brew cask install the-unarchiver
+brew cask install ifunbox
+brew cask install coconutbattery
+brew cask install unetbootin
 brew cask install vlc
 brew cask install dropbox
-brew cask install teamspeak-client
-brew cask install virtualbox
-brew cask install skype
-brew cask install teamviewer
 brew cask install spectacle
+# brew cask install cyberduck
+
+# Programming
+brew cask install sublime-text
+brew cask install typora
+# brew cask install netbeans
+
+# Browsers
+brew cask install google-chrome
+brew cask install opera
+
+# Socials
+brew cask install teamspeak-client
+brew cask install discord
+brew cask install skype
+
+# Downloads
+brew cask install transmission
+brew cask install jdownloader
+
+# Remote Control
+brew cask install vnc-viewer
+brew cask install teamviewer
+
+# Virtualization
+brew cask install virtualbox
+
+# Games
+brew cask install battle-net
+
+# Cleanup
 brew cleanup
 
-duti -s com.sublimetext.3 .h all
-duti -s com.sublimetext.3 .c all
-duti -s com.sublimetext.3 .cpp all
-duti -s com.sublimetext.3 .hpp all
-duti -s com.sublimetext.3 .sh all
-duti -s org.videolan.vlc .avi all
-duti -s org.videolan.vlc .flac all
-duti -s org.videolan.vlc .flv all
-duti -s org.videolan.vlc .m4a all
-duti -s org.videolan.vlc .mkv all
-duti -s org.videolan.vlc .mov all
-duti -s org.videolan.vlc .mp3 all
-duti -s org.videolan.vlc .mp4 all
-duti -s org.videolan.vlc .mpg all
-duti -s org.videolan.vlc .wav all
-duti -s org.videolan.vlc .wmv all
+# Association extension file to apps
+duti ./hidden/.duti
