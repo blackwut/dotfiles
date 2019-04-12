bb2c4b99370cdb37aad83ddb6eafcb5bb7b3bea2
blackwut <albertoottimo@gmail.com>
2019-04-12 17:08:54 +0200

    Guide to remove sleepimage from SSD

---
+++ sleepimage.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+# Restart your machine and hold down CMD + R to boot into recovery mode.
+# Type the following to disable SIP
+#
+#     csrutil disable
+#     reboot
+#
+# When your machine starts back up, go into terminal and remove the sleepimage.
+# This time, it’ll work.
+
+# Remove the sleep image file to save disk space
+sudo rm -f /private/var/vm/sleepimage
+# Create a zero-byte file instead
+sudo touch /private/var/vm/sleepimage
+# and make sure it can’t be rewritten
+sudo chflags uchg /private/var/vm/sleepimage
+
+# Now boot back into recovery mode and re-enable SIP:
+#     csrutil enable
+#     reboot
+#
+# When you’re back into MacOS verify that SIP is enabled and
+# the sleepimage has a size of 0 bytes.
+#     csrutil status
+#     ls -la /private/var/vm
