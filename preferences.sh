87a57e03043938ba540d54da3ea7ddb54f89a4d2
blackwut <albertoottimo@gmail.com>
2020-01-08 16:37:09 +0100

    New preferences. Added Finder Sidebar editor

--- preferences.sh
+++ preferences.sh
@@ -5,6 +5,11 @@ if csrutil status | grep -q enabled; then
     exit
 fi
 
+if command -v dockutil; then
+    echo "Please install dockutil"
+    exit
+fi
+
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
@@ -44,7 +49,7 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user #
 # Enable subpixel font rendering on non-Apple LCDs
-defaults write NSGlobalDomain AppleFontSmoothing -int 1
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
 # Disable Resume system-wide
 defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 # Increase sound quality for Bluetooth headphones/headsets
@@ -54,6 +59,46 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 # Set Help Viewer windows to non-floating mode
 defaults write com.apple.helpviewer DevMode -bool true
+# Turn Bluetooth off.
+sudo defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0
+# Disable the system alert sound
+defaults write NSGlobalDomain com.apple.sound.beep.volume -int 0
+defaults write NSGlobalDomain com.apple.sound.uiaudio.enabled -int 0
+# Enable volume change feedback
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -bool true
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+# Show menu extras
+defaults write com.apple.systemuiserver menuExtras -array \
+    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+    "/System/Library/CoreServices/Menu Extras/Clock.menu" \
+    "/System/Library/CoreServices/Menu Extras/Battery.menu"
+
+# Setup the menu bar date format
+defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM  HH:mm:ss"
+# Flash the : in the menu bar
+defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
+# 24 hour time
+defaults write NSGlobalDomain AppleICUForce24HourTime -bool true
+
+# Shows ethernet connected computers in airdrop
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Disable all actions when inserting disks
+defaults write com.apple.digihub com.apple.digihub.blank.bd.appeared -dict-add action -int 1
+defaults write com.apple.digihub com.apple.digihub.blank.cd.appeared -dict-add action -int 1
+defaults write com.apple.digihub com.apple.digihub.blank.dvd.appeared -dict-add action -int 1
+defaults write com.apple.digihub com.apple.digihub.cd.music.appeared -dict-add action -int 1
+defaults write com.apple.digihub com.apple.digihub.dvcamera.IIDC.appeared -dict-add action -int 1
+defaults write com.apple.digihub com.apple.digihub.dvcamera.IIDC.irisopened -dict-add action -int 1
+defaults write com.apple.digihub com.apple.digihub.dvd.video.appeared -dict-add action -int 1
+
+# Enable the debug menu in Disk Utility
+defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+defaults write com.apple.DiskUtility advanced-image-options -bool true
 
 
 ###############################################################################
@@ -110,17 +155,29 @@ sudo defaults write /Library/Preferences/com.apple.TimeMachine DoNotOfferNewDisk
 ###############################################################################
 # Trackpad                                                                    #
 ###############################################################################
-# Trackpad: enable tap to click
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+# Enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-# Trackpad: map bottom right corner to right-click
+# Disable three finger tap
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerTapGesture -int 0
+# Trackpad: 'smart zoom' two finger double tap
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.twoFingerDoubleTapGesture -int 0
+# Trackpad: disable swipe from right to show notification center
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.twoFingerFromRightEdgeSwipeGesture -int 0
+# Mouse: TwoButton mouse
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse.plist MouseButtonMode -string "TwoButton"
+# Map bottom right corner to right-click [To be checked]
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 # Disable “natural” (Lion-style) scrolling
 defaults write -g com.apple.swipescrolldirection -bool false
+# Disable El Capitan shake to magnify cursor
+defaults write NSGlobalDomain CGDisableCursorLocationMagnification -bool true
 
 
 ###############################################################################
@@ -146,8 +203,8 @@ defaults -currentHost write com.apple.universalaccess closeViewScrollWheelToggle
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 
 ###############################################################################
@@ -163,6 +220,12 @@ defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 # Desktop Show Removable Media
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Allow text selection in QuickLook
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Don't use tabs in Finder
+defaults write com.apple.finder AppleWindowTabbingMode -string "manual"
 
 # Show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
@@ -206,6 +269,10 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 64" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 64" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 64" ~/Library/Preferences/com.apple.finder.plist
+# Remove all tags from contextual menu
+/usr/libexec/PlistBuddy -c "Delete :FavoriteTagNames" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Add :FavoriteTagNames array" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Add :FavoriteTagNames:0 string" ~/Library/Preferences/com.apple.finder.plist
 
 # Use list view in all Finder windows by default
 # Flwv: Cover Flow View
@@ -214,12 +281,29 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # icnv: Icon View
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
+# Setting up Finder Sidebar
+python - <<EOF
+from FinderSidebarEditor import FinderSidebar
+sidebar = FinderSidebar()
+sidebar.removeAll()
+sidebar.add("/Users/blackwut/Projects")
+sidebar.add("/Users/blackwut/Dropbox")
+sidebar.add("/Applications")
+sidebar.add("/Users/blackwut")
+sidebar.add("/Volumes/RamDisk")
+sidebar.add("/Volumes/Mojave")
+EOF
+
+# Removing .DS_Store files
+echo "Removing .DS_Store files everywhere to update PreferredViewStyle in all directories... (this may take a while)"
+sudo find / -name .DS_Store -delete 2> /dev/null
+
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
 # Don’t group windows by application in Mission Control
 defaults write com.apple.dock expose-group-by-app -bool false
-# Don't show Dashboard as a Space
-defaults write com.apple.dock dashboard-in-overlay -bool true
+# Show Dashboard as a Space
+defaults write com.apple.dashboard db-enabled-state -int 3
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 # Don’t show recent applications in Dock
@@ -246,6 +330,11 @@ defaults write com.apple.dock wvous-tr-modifier -int 0
 defaults write com.apple.dock wvous-br-corner -int 4
 defaults write com.apple.dock wvous-br-modifier -int 0
 
+# Install iStatPro and configure it
+cp -r iStatPro.wdgt ~/Library/Widgets/iStatPro.wdgt
+plutil -convert binary1 -o - "./preferences/widget-com.iSlayer.iStatpro4.widget.plist" > ~/"Library/Preferences/widget-com.iSlayer.iStatpro4.widget.plist"
+plutil -convert binary1 -o - "./preferences/com.apple.dashboard.plist" > ~/"Library/Preferences/com.apple.dashboard.plist"
+
 
 ###############################################################################
 # Dock                                                                        #
@@ -262,6 +351,27 @@ defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 defaults write com.apple.dock autohide -bool false
 # Set Dock zoom size
 defaults write com.apple.dock magnification -int 0
+
+# Rearrange icons in dock
+dockutil --remove all
+dockutil --add "/Applications/Safari.app"
+dockutil --add "/Applications/Telegram Desktop.app"
+dockutil --add "/Applications/Discord.app"
+dockutil --add "/Applications/TextEdit.app"
+dockutil --add "/Applications/Notes.app"
+dockutil --add "/Applications/Calendar.app"
+dockutil --add "/Applications/Maps.app"
+dockutil --add "/Applications/iTunes.app"
+dockutil --add "/Applications/App Store.app"
+dockutil --add "/Applications/System Preferences.app"
+dockutil --add "/Applications/Utilities/Terminal.app"
+dockutil --add "/Applications/HSTracker.app"
+dockutil --add "/Applications/Battle.net.app"
+dockutil --add "/Applications/Sublime Text.app"
+dockutil --add "/Applications" --view grid --display folder --sort name
+dockutil --add "$HOME/Projects" --view grid --display folder --sort name
+dockutil --add "$HOME/Downloads" --view grid --display folder --sort dateadded
+
 
 
 ###############################################################################
@@ -282,7 +392,7 @@ defaults write com.apple.spotlight orderedItems -array \
     '{"enabled" = 1; "name" = "PDF";}' \
     '{"enabled" = 1; "name" = "MENU_EXPRESSION";}' \
     '{"enabled" = 1; "name" = "MENU_DEFINITION";}' \
-    '{"enabled" = 1; "name" = "SOURCE";}' \
+    '{"enabled" = 0; "name" = "SOURCE";}' \
     '{"enabled" = 0; "name" = "MENU_OTHER";}' \
     '{"enabled" = 0; "name" = "MENU_SPOTLIGHT_SUGGESTIONS";}' \
     '{"enabled" = 0; "name" = "MENU_CONVERSION";}' \
@@ -369,6 +479,13 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
 
 ###############################################################################
+# iTunes                                                                      #
+###############################################################################
+# stop running an automatic backup when you plug in an iOS device
+defaults write com.apple.iTunes AutomaticDeviceBackupsDisabled -bool true
+
+
+###############################################################################
 # Transmission.app                                                            #
 ###############################################################################
 # Use `/Volumes/RamDisk` to store incomplete downloads
@@ -390,6 +507,17 @@ defaults write org.m0k.transmission BlocklistURL -string "http://john.bitsurge.n
 defaults write org.m0k.transmission BlocklistAutoUpdate -bool true
 # Randomize port on launch
 defaults write org.m0k.transmission RandomPort -bool true
+
+
+###############################################################################
+# Updates                                                                     #
+###############################################################################
+# Disable automatic update & download
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool false
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -bool false
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticallyInstallMacOSUpdates -bool false
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate CriticalUpdateInstall -bool false
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -bool 
 
 
 ###############################################################################
